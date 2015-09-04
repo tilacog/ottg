0241ca5c3bbaf49b67ad59223e164689ec982cf3
Tiago Guimaraes <tilacog@gmail.com>
2015-09-04 15:45:20 -0300

    working authentication backend for Mozilla Personas

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 
+
 def persona_login(request):
-    authenticate(assertion=request.POST['assertion'])
-    return HttpResponse()
+    # This is an Ajax view, so it does't need to return HTMK, just an 'OK'
+    # string will do.
+    user = authenticate(assertion=request.POST['assertion'])
+    if user:
+        login(request, user)
+    return HttpResponse('OK')
