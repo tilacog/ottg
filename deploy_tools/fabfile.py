8bae3596c02e270ed88d7b41483ee7e1474bba54
Tiago Guimaraes <tilacog@gmail.com>
2015-09-05 18:47:52 -0300

    fixed typo

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -30,7 +30,6 @@ def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, 'DEBUG = True', 'DEBUG = False')
     sed(settings_path, 'DOMAIN = "localhost"', 'DOMAIN = "%s"' % (site_name,))
-                        DOMAIN = 'localhost'
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
