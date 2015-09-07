fd38a1f1960e36977640dff1c1ba06dc0945cf7b
Tiago Guimaraes <tilacog@gmail.com>
2015-09-07 12:34:33 -0300

    Trying to authenticate session on the staging site

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,45 +1,22 @@
 from django.conf import settings
-from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
-from django.contrib.sessions.backends.db import SessionStore
-
 from .base import FunctionalTest
+from .server_tools import create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
 
 
-"""
-The intent of this test is set up a pre-authenticated session, using cookies.
-(TDD with Python, page 304)
-
-We create a session object in the database. (The session key is the PK of the
-user object.)
-
-We then add a cookie to the browser that matches the session on the server, so
-on our next visit to the site, the server should recognize us as a logged-in
-user.
-
-Note tat, as it is, this will only work because FunctionalTest inherits from
-LiveServerTestCase, so the User and Session objects we create will end up in
-the same database as the test server.
-
-Later we'll need to modify it so that it works against the datbase of the
-staging server too.
-
-"""
-
-User = get_user_model()
 class MyListsTest(FunctionalTest):
   
     def create_pre_authenticated_session(self, email):
-        user = User.objects.create(email=email)
-        session = SessionStore()
-        session[SESSION_KEY] = user.pk
-        session[BACKEND_SESSION_KEY] =  settings.AUTHENTICATION_BACKENDS[0]
-        session.save()
+        if self.against_staging:
+            session_key = create_session_on_server(self.server_host, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
         ## to set a cookie we need to first visit the domain.
         ## 404 pages load the quickest!
         self.browser.get(self.server_url + "/404_no_such_url")
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
-            value=session.session_key,
+            value=session_key,
             path='/',
         ))
 
